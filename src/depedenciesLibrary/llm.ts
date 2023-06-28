e61ed7879860d9ad6fe2eee4acf18920113d5527
TDM <cto.junioor@gmail.com>
2023-06-28 17:34:53 +0530

    WIP: Allow users to specify character persona

--- src/depedenciesLibrary/llm.ts
+++ src/depedenciesLibrary/llm.ts
@@ -1,14 +1,51 @@
 import axios from 'axios';
+import { StringMappingType } from 'typescript';
 
-const API_URL = 'http://127.0.0.1:8080'
+//TODO: Format Names should ideally be enums
+const formatPrompt = (prompt: string, input: string, personaConfig: string): string => {
+  if (personaConfig !== undefined) {
+      const personaConfigJSON = JSON.parse(personaConfig);
+      const charName = personaConfigJSON.name || personaConfigJSON.char_name;
+      const charPersona  = personaConfigJSON.description || personaConfigJSON.char_persona;
 
-export const llamaInvoke = (prompt: string, input: string, onDataFunction: (data: string) => void): Promise<string> => {
-  const formattedPrompt = `### Instruction:\n ${prompt} \n ### Input:\n ${input} \n ### Response:\nbob:\n`;
+      var cleanPrompt = prompt + charPersona;
+
+      var re = /alice/gi;
+      var cleanInput = input.replace(re, "You");
+
+      var re = /bob/gi;
+      var cleanInput = cleanInput.replace(re, charName);
+
+      var exampleDialogues = personaConfigJSON.example_dialogue;
+      exampleDialogues = exampleDialogues.replace(/{{char}}/g, charName);
+      exampleDialogues = exampleDialogues.replace(/{{user}}/g, "You");      
+
+      cleanPrompt = `${cleanPrompt}\n<START>\n${personaConfigJSON.scenario}\n${exampleDialogues}`
+      return `${charName}'s Persona: ${cleanPrompt}\n<START>\n${cleanInput}\n${charName}:`
+  } else {
+    return `### Instruction:\n ${prompt} \n ### Input:\n ${input} \n ### Response:\nbob:\n`;
+  }
+}
+
+export const llamaInvoke = (prompt: string, input: string, llamaServerUrl: string, personConfig:string, onDataFunction: (data: string) => void): Promise<string> => {
+  let formattedPrompt:string;
+  formattedPrompt = formatPrompt(prompt, input, personConfig);
+  let stopTokens:string[] = [];
+  if (personConfig !== undefined) {
+    const personaConfigJSON = JSON.parse(personConfig);
+    const charName = personaConfigJSON.name || personaConfigJSON.char_name;
+    stopTokens.push(`${charName}:`);
+    stopTokens.push(`Alice:`);
+    stopTokens.push(`Alice  `);
+    stopTokens.push(`alice:`);
+    stopTokens.push(`alice  `);
+  }
+
   let answer = '';
   return new Promise(async (resolve, reject) => {
     const response = await axios({
       method: 'post',
-      url: `${API_URL}/completion`,
+      url: `${llamaServerUrl}/completion`,
       data: {
         prompt: formattedPrompt,
         temperature: 0.7,
@@ -16,7 +53,8 @@ export const llamaInvoke = (prompt: string, input: string, onDataFunction: (data
         top_p: 0.9,
         repeat_penalty: 1.3,
         // n_predict: 256,
-        stream: true
+        stream: true,
+        stop: stopTokens,
       },
       responseType: 'stream',
     });
